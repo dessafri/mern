95e8372552315b98266695c35ac7f0c96c2f4ca9
dessafri <41738535+dessafri@users.noreply.github.com>
2021-08-08 16:12:30 +0700

    add header in landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import LandingPage from 'pages/LandingPage'
+import 'assets/scss/style.scss'
 function App() {
   return (
     <div className="App">
-      Hai World!
+      <Router>
+        <Route path="/" component={LandingPage}></Route>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/parts/iconText.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from 'elements/button'
+
+const iconText = () => {
+  return (
+    <Button className="brand-text-icon" href="" type="Link">
+      Stay<span className="text-gray-900">cation.</span>
+    </Button>
+  )
+}
+
+export default iconText
